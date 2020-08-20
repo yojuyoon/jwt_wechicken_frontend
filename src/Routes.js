3ff12991090cd1adfc91849b12b2585725069b84
hyeonjiya <imhyeonji0118@gmail.com>
2020-08-20 22:09:37 +0900

    Add: Nav 레이아웃 완성

--- src/Routes.js
+++ src/Routes.js
@@ -1,9 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
+import Nav from "./Components/Nav";
 
 class Routes extends React.Component {
   render() {
-    return <Router></Router>;
+    return (
+      <Router>
+        <Nav />
+      </Router>
+    );
   }
 }
 export default Routes;
